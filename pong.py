f46c763135c8ff4701f15b8cf0fa8692415b2b67
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-11-08 15:01:24 +0000

    seems to work, use echo-client to send test data

--- pong.py
+++ pong.py
@@ -1,48 +1,62 @@
 import pygame, sys, random
+import socket
 
 def ball_animation():
-	global ball_speed_x, ball_speed_y
-	
-	ball.x += ball_speed_x
-	ball.y += ball_speed_y
+    global ball_speed_x, ball_speed_y
+    
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-	if ball.top <= 0 or ball.bottom >= screen_height:
-		ball_speed_y *= -1
-	if ball.left <= 0 or ball.right >= screen_width:
-		ball_start()
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_start()
 
-	if ball.colliderect(player) or ball.colliderect(opponent):
-		ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
 
-def player_animation():
-	player.y += player_speed
+def player_animation(speed):
+    player.y += speed
 
-	if player.top <= 0:
-		player.top = 0
-	if player.bottom >= screen_height:
-		player.bottom = screen_height
+    if player.top <= 0:
+        player.top = 0
+        return 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+        return 0
+
+    return speed
 
 def opponent_ai():
-	if opponent.top < ball.y:
-		opponent.y += opponent_speed
-	if opponent.bottom > ball.y:
-		opponent.y -= opponent_speed
+    if opponent.top < ball.y:
+        opponent.y += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.y -= opponent_speed
 
-	if opponent.top <= 0:
-		opponent.top = 0
-	if opponent.bottom >= screen_height:
-		opponent.bottom = screen_height
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 def ball_start():
-	global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y
 
-	ball.center = (screen_width/2, screen_height/2)
-	ball_speed_y *= random.choice((1,-1))
-	ball_speed_x *= random.choice((1,-1))
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
 
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.bind((HOST, PORT))
+my_socket.listen()
+connection, address = my_socket.accept()
 
 # Main Window
 screen_width = 1280
@@ -62,36 +76,36 @@ opponent = pygame.Rect(10, screen_height / 2 - 70, 10,140)
 # Game Variables
 ball_speed_x = 7 * random.choice((1,-1))
 ball_speed_y = 7 * random.choice((1,-1))
-player_speed = 0
 opponent_speed = 7
+player_speed = 0
 
+target_y = 600
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				player_speed -= 6
-			if event.key == pygame.K_DOWN:
-				player_speed += 6
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_UP:
-				player_speed += 6
-			if event.key == pygame.K_DOWN:
-				player_speed -= 6
-	
-	#Game Logic
-	ball_animation()
-	player_animation()
-	opponent_ai()
+    
+    data = connection.recv(256)
+    if data:
+        try:
+            target_y = int(data.decode())
+        except:
+            pass
 
-	# Visuals 
-	screen.fill(bg_color)
-	pygame.draw.rect(screen, light_grey, player)
-	pygame.draw.rect(screen, light_grey, opponent)
-	pygame.draw.ellipse(screen, light_grey, ball)
-	pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+    if target_y > player.y:
+        player_speed += 1
+    elif target_y < player.y:
+        player_speed -= 1
+    else:
+        player_speed =0
+    #Game Logic
+    ball_animation()
+    player_speed = player_animation(player_speed)
+    opponent_ai()
 
-	pygame.display.flip()
-	clock.tick(60)
+    # Visuals 
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+
+    pygame.display.flip()
+    clock.tick(60)
